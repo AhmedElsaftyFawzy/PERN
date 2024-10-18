71d78f2564e1ee1036705fcc0987fd241ad4e2a3
AhmedElsaftyFawzy <113257878+AhmedElsaftyFawzy@users.noreply.github.com>
2024-10-18 16:00:36 +0300

    main

--- Social Media App/server/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(225) NOT NULL,
-    last_name VARCHAR(225) NOT NULL,
-    email VARCHAR(225) NOT NULL,
-    password VARCHAR(450) NOT NULL,
-    reset_password_token VARCHAR(225),
-    reset_password_expires TIMESTAMP
-)
